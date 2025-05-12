fd7f635d6d888a20887560bb6a0a1f160df6611c
keserarda35 <keserarda35@gmail.com>
2025-05-13 01:20:47 +0300

    added categories and meals screens

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -5,31 +5,43 @@ class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
     super.key,
     required this.category,
+    required this.onSelectCategory,
   });
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
+    return InkWell(
+      onTap: onSelectCategory,
 
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withValues(alpha: .5),
-            category.color.withValues(alpha: .9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+      splashColor: Theme.of(context).primaryColor,
+
+      borderRadius: BorderRadius.circular(16),
+
+      child: Container(
+        padding: EdgeInsets.all(16),
+      
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: .5),
+              category.color.withValues(alpha: .9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-
-      child: Text(
-        category.title,
-
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onSurface
+      
+        child: Text(
+          category.title,
+      
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface
+          ),
         ),
       ),
     );
